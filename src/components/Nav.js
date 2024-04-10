6cf6d3fe101d97fa44ae385c9fda1f005f72151b
kanishq_77 <ashakanishq77@gmail.com>
2024-04-10 03:32:31 -0700

    build

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -25,7 +25,7 @@ const Nav = () => {
 						to="/Home"
 						className="flex items-center space-x-3 rtl:space-x-reverse"
 					>
-						<i className="fa-solid fa-book	"></i>
+						<i className="fa-solid fa-book "></i>
 
 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">
 							iNotes
@@ -57,7 +57,9 @@ const Nav = () => {
 						</svg>
 					</button>
 					<div
-						className="hidden w-full md:block md:w-auto"
+						className={`${
+							isNavbarOpen ? "block" : "hidden"
+						} w-full md:block md:w-auto`}
 						id="navbar-default"
 					>
 						<ul className="font-medium flex flex-col p-4 md:p-0 mt-4  bg-lime-400 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-lime-400 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
